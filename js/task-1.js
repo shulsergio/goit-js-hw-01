ab258ebc6e78fb850c5ba07fb2e12b25df82041b
Sergey Shulga <shul.sergio@gmail.com>
2024-04-24 10:13:02 +0300

    v.1.3

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,7 @@
-console.log("1");
+function makeTransaction(quantity, pricePerDroid) {
+  let totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
